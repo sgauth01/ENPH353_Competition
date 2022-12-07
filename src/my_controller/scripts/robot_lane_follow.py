9c3dc42fe03a59292d9dae91f650f2eec2f40f05
sgauth01 <90274880+sgauth01@users.noreply.github.com>
2022-12-06 23:18:52 -0800

    Update robot_lane_follow.py

--- src/my_controller/scripts/robot_lane_follow.py
+++ src/my_controller/scripts/robot_lane_follow.py
@@ -13,9 +13,6 @@ import time
 
 initPlate = 'TeamTS,bear,0,XR58'
 finalPlate = 'TeamTS,bear,-1,XR58'
-start_time = time.time()
-
-number_of_plates_tracked = 0
 
 class image_converter:
 
@@ -23,28 +20,31 @@ class image_converter:
     self.command_pub = rospy.Publisher("/R1/cmd_vel", Twist, queue_size=1)
 
     self.bridge = CvBridge()
-    self.image_sub = rospy.Subscriber("/R1/pi_camera/image_raw", Image, self.callback),
+    self.image_sub = rospy.Subscriber("/R1/pi_camera/image_raw", Image, self.callback)
 
     self.plate_pub = rospy.Publisher("/license_plate", String, queue_size=1)
-    
-  
+
+    self.start_time = time.time()
+    self.number_of_plates_tracked = 0
+    self.detected_time = 0
+    self.pedestrian_detected = False
+    self.state = 0
+    self.crosswalk_init_time = 0
+    self.num_crosswalks_detected = 0
+    self.last_crosswalk_time = 0
+    self.last_plate_published = False
+
+    # decide which line of the road to follow (0 is left and 1 is right)
+    self.line_to_follow = 0
+
+    # self.plate_pub = rospy.Publisher("/license_plate", String, queue_size=1)
+
   def callback(self,data):
     try:
       cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
     except CvBridgeError as e:
       print(e)
 
-    # after number of plates tracked = 1, switch to following right line (from left line)
-    # for now let's just say that when 5 seconds have passed we switch the line
-
-    # decide which line of the road to follow (0 is left and 1 is right)
-    line_to_follow = 0
-
-    current_time = time.time()
-
-    if (current_time - start_time > 8):
-      line_to_follow = 1
-    
     # convert image to grayscale
     gray_frame = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
 
@@ -55,8 +55,7 @@ class image_converter:
     height = gray_frame.shape[0]
     width = gray_frame.shape[1]
 
-    # convert grayscale image to binary scale
-    (thresh, binary_img) = cv2.threshold(gray_frame, 180, 255, cv2.THRESH_BINARY)
+    move = Twist()
 
     # convert hsv image to binary scale
     binary_hsv = cv2.inRange(hsv_frame, (0, 0, 100), (0, 0, 255))
@@ -64,78 +63,154 @@ class image_converter:
     # blur image
     binary_img = cv2.blur(binary_hsv,(5,5))
 
-    row = binary_img[600]
+    if (self.state == 0):
 
-    move = Twist()
-    kp = 4
+      # STATE: DRIVING AROUND OUTER LOOP
 
-    leading_edge = 0
-    origin = width/2
-    move.linear.x = 0.2
-    threshold = 200
-    sum = 0
+      self.pedestrian_detected = False
 
-    # detect the edge of the line on the left side
-    if (line_to_follow == 0):
-      column = 0
-      for pixel in row:
-        column += 1
-        if (pixel > threshold):
-          # making sure there are at least 5 white pixels in a row and not just random noisy pixels
-          sum += 1
-        else:
-          sum = 0
-        if (sum >= 5):
-          leading_edge = column - sum
-          break
+      current_time = time.time()
 
-      # find the center pixel
-      center = leading_edge + 400
-      #find how much turning is needed
-      move.angular.z = (1-center/origin)*kp
-    
-    # detect the edge of the line on the right side
-    else:
-      for i in range(1, len(row)):
-        column = width - i
-        if (row[column] > threshold):
-          sum += 1
-        else:
-          sum = 0
-        if (sum >= 5):
-          leading_edge = column + sum
-          break
+      if (current_time - self.start_time > 6):
+        self.line_to_follow = 1
+
+      row = binary_img[600]
+
+      kp = 4
+
+      leading_edge = 0
+      origin = width/2
+      move.linear.x = 0.2
+      threshold = 200
+      sum = 0
+
+      # detect the edge of the line on the left side
+      if (self.line_to_follow == 0):
+        column = 0
+        for pixel in row:
+          column += 1
+          if (pixel > threshold):
+            # making sure there are at least 5 white pixels in a row and not just random noisy pixels
+            sum += 1
+          else:
+            sum = 0
+          if (sum >= 5):
+            leading_edge = column - sum
+            break
+
+        # find the center pixel
+        center = leading_edge + 400
+        #find how much turning is needed
+        move.angular.z = (1-center/origin)*kp
       
-      # find the center pixel
-      center = leading_edge - 400 
-      # find how much turning is needed
-      move.angular.z = (1-center/origin)*kp 
+      # detect the edge of the line on the right side
+      else:
+        for i in range(1, len(row)):
+          column = width - i
+          if (row[column] > threshold):
+            sum += 1
+          else:
+            sum = 0
+          if (sum >= 5):
+            leading_edge = column + sum
+            break
+        
+        # find the center pixel
+        center = leading_edge - 400 
+        # find how much turning is needed
+        move.angular.z = (1-center/origin)*kp 
+
+      # TRYING TO DETECT CROSSWALK:
+
+      # filter out every colour except red
+      red_mask = cv2.inRange(hsv_frame, (0, 200, 0), (6, 255, 255))
+      red_blurry = cv2.blur(red_mask,(5,5))
+
+      M = cv2.moments(red_blurry)
+      try:
+        cY = int(M["m01"] / M["m00"])
+      except ZeroDivisionError:
+        cY = 0
+      
+      current_time = time.time()
+
+      if (cY > 500) and (current_time - self.detected_time > 5):
+        self.state = 1 # crosswalk state
+        self.crosswalk_init_time = time.time()
+      
+      # FINDING TIME ATER TWO CROSSWALKS WERE DETECTED
+      if (self.num_crosswalks_detected == 2):
+        self.last_crosswalk_time = time.time()
+        self.num_crosswalks_detected += 1
+      
+      # STOPPING SIMULATION AFTER A FEW SECONDS OF TWO CROSSWALKS BEING DETECTED
+      if (current_time - self.last_crosswalk_time > 10) and (self.num_crosswalks_detected == 3):
+        # stop moving
+        move.linear.x = 0
+        move.angular.z = 0
+        center = 0
+        if (self.last_plate_published == False):
+          self.plate_pub.publish(finalPlate)
+          self.last_plate_published = True
+      
+    # STATE: CROSSWALK
+    elif (self.state == 1):
+      # stop moving
+      move.linear.x = 0
+      move.angular.z = 0
+      center = 0
+
+      cropped = binary_img[360:380,650:670]
+      cv2.imshow("Cropped", cropped)
+      cv2.waitKey(3)
+
+      # try to detect pedestrian
+      average_colour = np.mean(cropped)
+
+      if average_colour > 5:
+        self.pedestrian_detected = True
+        print("Pedestrian Detected")
+        self.state = 0
+        self.num_crosswalks_detected += 1
+        self.detected_time = time.time()
+      
+      current_time = time.time()
+      if ((current_time - self.crosswalk_init_time) > 8):
+        self.detected_time = time.time()
+        self.num_crosswalks_detected += 1
+        self.state = 0
 
     # highlight the center of the road
 
-    cv2.circle(binary_img, (center, 600), 5, (255, 255, 255), -1)
-    cv2.putText(binary_img, "center", (center - 25, 600 - 25),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+    cv2.circle(cv_image, (center, 600), 5, (255, 255, 255), -1)
+    cv2.putText(cv_image, "center", (center - 25, 600 - 25),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
 
     # write at top left of image which line is being followed
 
-    if (line_to_follow == 0):
-      cv2.putText(binary_img, "line following: left", (10,10),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+    if (self.line_to_follow == 0):
+      cv2.putText(cv_image, "line following: left", (10,10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
     else:
-      cv2.putText(binary_img, "line following: right", (10,10),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+      cv2.putText(cv_image, "line following: right", (10,10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+    
+    if (self.state == 0):
+      cv2.putText(cv_image, "state: driving", (10,30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+    elif (self.state == 1):
+      cv2.putText(cv_image, "state: crosswalk", (10,30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+    
+    if (self.pedestrian_detected):
+      cv2.putText(cv_image, "PEDESTRIAN DETECTED!!!!", (10,50), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
 
-    # display the binary HSV image
-    cv2.imshow('Binary HSV', binary_img)
+    # display the car mask
+
+    # display the normal CV image
+    cv2.imshow('CV Image', cv_image)
     cv2.waitKey(3)
-
-    # after 30 seconds have passed, stop timer
-    current_time = time.time()
-    if (current_time - start_time) > 30 and (current_time - start_time) < 31:
-      self.plate_pub.publish(finalPlate)
 
     try:
       self.command_pub.publish(move)
     except CvBridgeError as e:
       print(e)
+
 
 def main(args):
   ic = image_converter()
